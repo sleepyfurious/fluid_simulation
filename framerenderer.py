eb6b1f8de86671d51bd9062403c500a1f2787186
Sleepy Pro <sleepyfurious@gmail.com>
2016-02-11 23:48:31 +0700

    the grid of vector field visualizer

--- framerenderer.py
+++ framerenderer.py
@@ -44,19 +44,49 @@ class FrameRenderer:
 
         glClearBufferfv( GL_COLOR, 0, ( 1,.5,.5,1 ) )
 
-        glPointSize( 40.0 )
+        glPointSize( 2.0 )
 
         glUseProgram( self.program )
 
         glBindVertexArray( self.blankVao )
-        glDrawArrays( GL_POINTS, 0, 1 )
+        glDrawArrays( GL_LINES, 0, 100*2 )
         glBindVertexArray( 0 )
 
 _velocityLineVertShader = """
 #version 330 core
+#line 58
+
+/* confuse how, but taken from here:
+ * http://stackoverflow.com/questions/14845084/how-do-i-convert-a-1d-index-into-a-3d-index
+ * look like author tell it wrong ( round -> floor ) */
+ivec3 GetIndex3Dfrom1D( int i, ivec2 wh ) {
+    int     wXh     = wh.x *wh.y;
+    float   rWxH    = 1.0 /wXh;
+    float   rW      = 1.0 /wh.x;
+    int z = int( i *rWxH );
+    int y = int( ( i -z*wXh ) *rW );
+    int x = i -wh.x *( y +wh.y*z );
+    return ivec3( x, y, z );
+}
+
+// unit: meter
+uniform float gridSpacing = 0.1;
+uniform ivec3 gridN = ivec3( 10 );
 
 void main ( void ) {
-    gl_Position = vec4( 0, 0, 0.5, 1 );
+    int     cellID = int( gl_VertexID *0.5 );
+    bool    isTail  = mod( gl_VertexID, 2 ) == 0.0; // isOdd 0---->1
+
+    vec3    gridSize    = gridSpacing *gridN;
+    ivec3   gridCoord   = GetIndex3Dfrom1D( cellID, gridN.xy );
+    vec3    gridPos     = ( gridSize *gridCoord ) / gridN;
+    vec3    centeredGridPos = gridPos -( 0.5 *gridSpacing *( gridN -1 ) );
+
+    if ( isTail ) {
+        centeredGridPos += 0.05;
+    }
+
+    gl_Position = vec4( centeredGridPos, 1 );
 }
 """
 
